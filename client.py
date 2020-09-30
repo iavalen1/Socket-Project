cecc394cbba94dab96320efe6f71de0b3259473a
iavalen1 <61523383+iavalen1@users.noreply.github.com>
2020-09-30 09:22:51 -0700

    Project Milestone
    These are the files with the functionality required for the milestone

--- client.py
+++ client.py
@@ -2,64 +2,204 @@ import sys
 import socket
 import pickle
 import threading
+import argparse
 import time
 
-CLIENT_NAME = None
-CLIENT_ADDRESS = None
-CLIENT_PORTS = []
-CLIENT_LISTEN_PORT = None
-SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
-SERVER_PORT = 47000
-RING_SIZE = None
-RING_ID = None
-RING_LEADER_NAME = None
-RING_LEADER_PORT = None
-SEND_MESSAGE = None
-RECIEVED_MESSAGE = None
+# Handle command-line arguments(argparser)
+parser = argparse.ArgumentParser(description = 'Initialize a client')
+parser.add_argument('-n', '--name', type=str, metavar='', required=True, help='Client name(maximum 15 characters)')
+parser.add_argument('-cip', '--client_ip', type=str, metavar='', required=False, default=socket.gethostbyname(socket.gethostname()), help='Client IP address')
+parser.add_argument('-sip', '--server_ip', type=str, metavar='', required=False, default=socket.gethostbyname(socket.gethostname()), help='Server IP address')
+parser.add_argument('-lcp', '--left_communication_port', type=int, metavar='', required=True, help='Client port number for left communication socket')
+parser.add_argument('-scp', '--server_communication_port', type=int, metavar='', required=True, help='Client port number for server communication socket')
+parser.add_argument('-rcp', '--right_communication_port', type=int, metavar='', required=True, help='Client port number for right communication socket')
+args = parser.parse_args()
 
-def createMessage(args):
-    if args[1] == "register":
-        CLIENT_NAME = args[2]
-        CLIENT_ADDRESS = args[3]
-        CLIENT_PORTS = [int(args[4]), int(args[5]), int(args[6])]
-        CLIENT_LISTEN_PORT = int(args[4])
-        SEND_MESSAGE = ["register", CLIENT_NAME, CLIENT_ADDRESS, CLIENT_PORTS]
-    if args[1] ==  "deregister":
-        CLIENT_NAME = args[2]
-        SEND_MESSAGE = ["deregister", CLIENT_NAME]
-    if args[1] == "setup-ring":
-        RING_SIZE = args[2]
-        CLIENT_NAME = args[3]
-        SEND_MESSAGE = ["setup-ring", RING_SIZE, CLIENT_NAME]
-    if args[1] == "setup-complete":
-        RING_ID = args[2]
-        RING_LEADER_NAME = args[3]
-        RING_LEADER_PORT = args[4]
-        SEND_MESSAGE = ["setup-complete", RING_ID, RING_LEADER_NAME, RING_LEADER_PORT]
-    if args[1] == "--help":
-        print("python client.py register <user-name> <IP-address> <port(s)>")
-        print("python client.py deregister <user-name>")
-        print("python client.py setup-ring <ring-size> <user-name>")
-        print("python client.py setup-complete <ring-id> <leader-user-name> <leader-port>")
+# Variables(initially instantiated)
+client_name = args.name
+client_ip = args.client_ip
+server_ip = args.server_ip
+server_port = 47000
+left_communication_port = args.left_communication_port
+server_communication_port = args.server_communication_port
+right_communication_port = args.right_communication_port
+
+# Variables(remaining)
+client_list = None
+rclient_ip = None
+rclient_port = None
+ring_id = None
+ring_size = None
+compute_port = None
+
+# Create sockets
+left_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+left_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+left_socket.bind((client_ip, left_communication_port))
+#left_socket.settimeout(5)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((client_ip, server_communication_port))
+#server_socket.settimeout(5)
+
+right_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+right_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+right_socket.bind((client_ip, right_communication_port))
+right_socket.settimeout(5)
+
+# Message handler function
+def message_handler(message):
+    global rclient_ip,rclient_port,ring_id,ring_size
+    
+    if message == "SUCCESS":
+        pass
+        #print("SUCCESS", flush=True)
+    elif message == "FAILURE":
+        pass
+        #print("FAILURE", flush=True)
+    elif message[0] == "SUCCESS" and len(message) == 5: # Initialize ring setup
+        # Store ring and index
+        ring_id, ring_size = message[1], message[2]
+        client_list = message[3]
+        next_index = message[4]
+        # Store right client information
+        rclient_ip = client_list[next_index][1]
+        rclient_port = client_list[next_index][2]
+        # Increment index
+        next_index += 1
+        # Create message
+        setup_message = pickle.dumps(["setup", client_list, next_index])
+        # Send message to right socket
+        client_rclient(setup_message)
+    elif message[0] == "setup": # During setup
+        client_list = message[1]
+        next_index = message[2]
+        if next_index == 1: # client is the leader
+            # Create setup-complete message
+            setup_complete_message = message_creator("setup-complete" + " " + ring_id + " " + client_name + " " + str(left_communication_port))
+            # send confirmation to server
+            server_socket.sendto(setup_complete_message, (server_ip, server_port))
+        else: # client is not leader
+            if next_index == len(client_list): # Final client
+                # Store right client information
+                rclient_ip = client_list[0][1]
+                rclient_port = client_list[0][2]
+                # Set nextindex to 1
+                next_index = 1
+            else: # Client isn't first or last 
+                # Store right client information
+                rclient_ip = client_list[next_index][1]
+                rclient_port = client_list[next_index][2]
+                # Increment index
+                next_index += 1
+            # Create message
+            setup_message = pickle.dumps(["setup", client_list, next_index])
+            # Send message to right socket
+            client_rclient(setup_message)
+
+# Message creator function
+def message_creator(command):
+    global ring_size,ring_id,compute_port
+    final_message = None
+    message = None
+
+    tmp_arr = command.split(" ")
+    funct = tmp_arr[0]
+
+    # Check first index for desired command and formats message accordingly
+    if funct == "exit":
         sys.exit()
+    elif funct == "register":
+        message = [funct, client_name, client_ip, left_communication_port, server_communication_port, right_communication_port]
+    elif funct == "deregister":
+        message = [funct, client_name]
+    elif funct == "setup-ring":
+        ring_size = int(tmp_arr[1])
+        message = [funct, ring_size, client_name]
+    elif funct == "setup-complete":
+        ring_id,compute_port = tmp_arr[1],int(tmp_arr[3])
+        message = [funct, ring_id, client_name, compute_port]
 
+    # Pickle the message
+    final_message = pickle.dumps(message)
+
+    # Return message
+    return final_message
+
+# Client-LClient function
+def client_lclient():
+    while(True):
+        # Recieve data
+        data, addr = left_socket.recvfrom(1024)
+        print("Message Recieved from " + str(addr), flush=True)
+
+        # Send acknowledgement
+        left_socket.sendto(pickle.dumps("SUCCESS"), addr)
+        print("Acknowledgement sent to " + str(addr), flush=True)
+
+        # Start thread for message handling
+        #message_handling_thread = threading.Thread(target=message_handler, args=[pickle.loads(data)])
+        #message_handling_thread.start()
+        #message_handling_thread.join()
+
+        message_handler(pickle.loads(data))
+
+# Client-RClient function
+def client_rclient(message):
+    # Send acknowledgement
+    print("Message sent to " + str((rclient_ip, rclient_port)), flush=True)
+    right_socket.sendto(message, (rclient_ip, rclient_port))
+
+    # Recieve data
+    data, addr = right_socket.recvfrom(1024)
+    print("Acknowledgement recieved from " + str(addr), flush=True)
+
+    # Handle message
+    message_handler(pickle.loads(data))
+
+# Client-Server function
+def client_server():
+    while(True):
+        # Create message from input command
+        message = message_creator(input())
+
+        # Send message to server
+        server_socket.sendto(message, (server_ip, server_port))
+
+        # Recieve message
+        data = server_socket.recv(1024)
+
+        # Start thread for message handling
+        #message_handling_thread = threading.Thread(target=message_handler, args=[pickle.loads(data)])
+        #message_handling_thread.start()
+
+        # Wait for server to respond  and send to message handler
+        message_handler(pickle.loads(data))
+
+# main function
 def main():
+    # Create threads for server and left sockets
+    server_thread = threading.Thread(target=client_server)
+    left_thread = threading.Thread(target=client_lclient)
 
-    print(socket.gethostbyname(socket.gethostname()))
-    createMessage(sys.argv)
+    try:
+        # Start execution of threads for server and left sockets
+        server_thread.start()
+        left_thread.start()
+    except KeyboardInterrupt:
+        # Close threads
+        server_thread.join()
+        left_thread.join()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((CLIENT_ADDRESS, CLIENT_PORTS[0]))
-    sock.settimeout(5)
-
-    serial_data = pickle.dumps(SEND_MESSAGE)
-
-    sock.sendto(serial_data, (SERVER_ADDRESS, SERVER_PORT))
-
-    RECIEVED_MESSAGE = sock.recvfrom(1024)[0].decode("utf-8")
-
-    print(RECIEVED_MESSAGE)
+        # Close sockets and exit
+        left_socket.shutdown(socket.SHUT_RDWR)
+        right_socket.shutdown(socket.SHUT_RDWR)
+        server_socket.shutdown(socket.SHUT_RDWR)
+        left_socket.close()
+        right_socket.close()
+        server_socket.close()
+        sys.exit()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,29 +1,100 @@
-#!python
-
 import sys
 import socket
+import pickle
 import threading
 import time
 
+# Variables
+server_ip = socket.gethostbyname(socket.gethostname())
+server_port = 47000
+free_clients = 0
+information_base = dict()
+ring_base = []
+
+# Create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((server_ip, server_port))
+sock.setblocking(0)
+
+# Message handling
+def message_handler(data, addr):
+    global free_clients
+    command = data[0]
+
+    if command == "register":
+        # Check if name is in information base
+        if data[1] in information_base:
+            sock.sendto(pickle.dumps("FAILURE"), addr)
+            print("Failed to register " + str(addr), flush=True)
+        else:
+            information_base[data[1]] = data[2:] + ["FREE"]
+            free_clients += 1
+            sock.sendto(pickle.dumps("SUCCESS"), addr)
+            print("Successfully registerd " + str(addr), flush=True)
+    elif command == "deregister":
+        if data[1] in information_base and information_base[data[1]][4] != "InRing":
+            del information_base[data[1]]
+            free_clients -= 1
+            sock.sendto(pickle.dumps("SUCCESS"), addr)
+            print("Successfully deregisterd " + str(addr), flush=True)
+        else:
+            sock.sendto(pickle.dumps("FAILURE"), addr)
+            print("Failed to deregister " + str(addr), flush=True)
+    elif command == "setup-ring":
+        if (data[2] in information_base) and (information_base[data[2]][4] == "FREE") and (data[1] >= 3) and (data[1]%2 != 0) and (free_clients-1 >= 2):
+            ring_data = ()
+            # Pick 3 clients to be part of ring (current client is first entry)
+            ring_data = [data[2]] + information_base[data[2]][:4],
+            information_base[data[2]][4] = "InRing"
+            for client,info in information_base.items():
+                if client != info[2] and info[4] == "FREE":
+                    ring_data += [client] + info[:4],
+                    info[4] = "InRing"
+                if ring_data.count == data[1]:
+                    break
+            # Decrease value of free_clients
+            free_clients -= data[1]
+            # Add to ring base
+            ring_base.append(["ring" + str(len(ring_base)), data[1], (data[2], "192.168.56.1",)])
+            # Create message
+            message = ["SUCCESS", ring_base[len(ring_base)-1][0], data[1], ring_data, 1]
+            # Send message
+            sock.sendto(pickle.dumps(message), addr)
+            print("Setting up ring with users: " + str(ring_data))
+        else:
+            sock.sendto(pickle.dumps("FAILURE"), addr)
+            print("Failed to setup ring!", flush=True)
+    elif command == "setup-complete":
+        # Update client state to Leader
+        information_base[data[2]][4] = "Leader"
+        # Store compute port in corresponding ring_base index
+        for ring in ring_base:
+            if ring[0] == data[1]:
+                ring[2] += data[3],
+        # Send success
+        sock.sendto(pickle.dumps("SUCCESS"), addr)
+        print("Succesfully set up ring with leader " + data[2], flush=True)
+
+
+# Wait for hosts and create threads
+def wait_for_clients():
+    while(True):
+        try:
+            data,addr = sock.recvfrom(1024)
+            message_handling_thread = threading.Thread(target=message_handler, args=(pickle.loads(data), addr))
+            message_handling_thread.start()
+        except BlockingIOError:
+            pass
+
+# main function
 def main():
-
-    SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
-    SERVER_PORT = 47000
-    CLIENT_PORT = None
-    CLIENT_ADDRESS = None
-    sendMessage = "SUCCESS"
-    messageRecieved = None
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((SERVER_ADDRESS, SERVER_PORT))
-    #sock.settimeout(5)
+    try:
+        wait_for_clients()
+    except KeyboardInterrupt:
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        sys.exit()
     
-    messageRecieved, (CLIENT_ADDRESS,CLIENT_PORT) = sock.recvfrom(1024)
-
-    print(messageRecieved.decode("utf-8"))
-
-    sock.sendto(sendMessage.encode("utf-8"), (CLIENT_ADDRESS, CLIENT_PORT))
-
 if __name__ == "__main__":
     main()
